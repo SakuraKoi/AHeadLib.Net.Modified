c789b241a32514c9aa0ea0f367767952b3e03b4c
sakurawa <root@sakuraawa.dev>
2024-06-07 14:18:29 +0800

    refactor: ui style

--- Sources/AHeadLib.Net/MainForm.Designer.cs
+++ Sources/AHeadLib.Net/MainForm.Designer.cs
@@ -35,92 +35,96 @@
             this.btnOutputDirectoryPick = new System.Windows.Forms.Button();
             this.editOutputDirectory = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textLog = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.dialogInputFile = new System.Windows.Forms.OpenFileDialog();
             this.editProjectName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.textLog = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 24);
+            this.label1.Size = new System.Drawing.Size(103, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Input File";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // editInputFile
             // 
-            this.editInputFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.editInputFile.Location = new System.Drawing.Point(128, 16);
+            this.editInputFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.editInputFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.editInputFile.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.editInputFile.Location = new System.Drawing.Point(120, 16);
+            this.editInputFile.Margin = new System.Windows.Forms.Padding(4);
             this.editInputFile.Name = "editInputFile";
-            this.editInputFile.Size = new System.Drawing.Size(392, 21);
+            this.editInputFile.Size = new System.Drawing.Size(472, 23);
             this.editInputFile.TabIndex = 1;
             // 
             // btnInputFilePick
             // 
-            this.btnInputFilePick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnInputFilePick.Location = new System.Drawing.Point(519, 15);
+            this.btnInputFilePick.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.btnInputFilePick.FlatAppearance.BorderSize = 0;
+            this.btnInputFilePick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInputFilePick.Location = new System.Drawing.Point(600, 16);
+            this.btnInputFilePick.Margin = new System.Windows.Forms.Padding(4);
             this.btnInputFilePick.Name = "btnInputFilePick";
-            this.btnInputFilePick.Size = new System.Drawing.Size(42, 23);
+            this.btnInputFilePick.Size = new System.Drawing.Size(49, 23);
             this.btnInputFilePick.TabIndex = 2;
             this.btnInputFilePick.Text = "...";
-            this.btnInputFilePick.UseVisualStyleBackColor = true;
+            this.btnInputFilePick.UseVisualStyleBackColor = false;
             this.btnInputFilePick.Click += new System.EventHandler(this.btnInputFilePick_Click);
             // 
             // btnOutputDirectoryPick
             // 
-            this.btnOutputDirectoryPick.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOutputDirectoryPick.Location = new System.Drawing.Point(519, 39);
+            this.btnOutputDirectoryPick.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.btnOutputDirectoryPick.FlatAppearance.BorderSize = 0;
+            this.btnOutputDirectoryPick.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOutputDirectoryPick.Location = new System.Drawing.Point(600, 48);
+            this.btnOutputDirectoryPick.Margin = new System.Windows.Forms.Padding(4);
             this.btnOutputDirectoryPick.Name = "btnOutputDirectoryPick";
-            this.btnOutputDirectoryPick.Size = new System.Drawing.Size(42, 23);
+            this.btnOutputDirectoryPick.Size = new System.Drawing.Size(49, 23);
             this.btnOutputDirectoryPick.TabIndex = 5;
             this.btnOutputDirectoryPick.Text = "...";
-            this.btnOutputDirectoryPick.UseVisualStyleBackColor = true;
+            this.btnOutputDirectoryPick.UseVisualStyleBackColor = false;
             this.btnOutputDirectoryPick.Click += new System.EventHandler(this.btnOutputDirectoryPick_Click);
             // 
             // editOutputDirectory
             // 
-            this.editOutputDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.editOutputDirectory.Location = new System.Drawing.Point(128, 40);
+            this.editOutputDirectory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.editOutputDirectory.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.editOutputDirectory.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.editOutputDirectory.Location = new System.Drawing.Point(120, 48);
+            this.editOutputDirectory.Margin = new System.Windows.Forms.Padding(4);
             this.editOutputDirectory.Name = "editOutputDirectory";
-            this.editOutputDirectory.Size = new System.Drawing.Size(392, 21);
+            this.editOutputDirectory.Size = new System.Drawing.Size(472, 23);
             this.editOutputDirectory.TabIndex = 4;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(16, 40);
+            this.label2.Location = new System.Drawing.Point(16, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 24);
+            this.label2.Size = new System.Drawing.Size(104, 23);
             this.label2.TabIndex = 3;
             this.label2.Text = "Output directory";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // textLog
-            // 
-            this.textLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textLog.Location = new System.Drawing.Point(16, 96);
-            this.textLog.Multiline = true;
-            this.textLog.Name = "textLog";
-            this.textLog.ReadOnly = true;
-            this.textLog.Size = new System.Drawing.Size(544, 129);
-            this.textLog.TabIndex = 6;
-            // 
             // btnGenerate
             // 
-            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnGenerate.Location = new System.Drawing.Point(464, 233);
+            this.btnGenerate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.btnGenerate.FlatAppearance.BorderSize = 0;
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerate.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnGenerate.Location = new System.Drawing.Point(520, 544);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(4);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(96, 24);
+            this.btnGenerate.Size = new System.Drawing.Size(131, 40);
             this.btnGenerate.TabIndex = 7;
             this.btnGenerate.Text = "Generate";
-            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.UseVisualStyleBackColor = false;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
             // dialogInputFile
@@ -129,27 +133,45 @@
             // 
             // editProjectName
             // 
-            this.editProjectName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.editProjectName.Location = new System.Drawing.Point(128, 64);
+            this.editProjectName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.editProjectName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.editProjectName.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.editProjectName.Location = new System.Drawing.Point(120, 80);
+            this.editProjectName.Margin = new System.Windows.Forms.Padding(4);
             this.editProjectName.Name = "editProjectName";
-            this.editProjectName.Size = new System.Drawing.Size(432, 21);
+            this.editProjectName.Size = new System.Drawing.Size(472, 23);
             this.editProjectName.TabIndex = 9;
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(16, 64);
+            this.label3.Location = new System.Drawing.Point(16, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 24);
+            this.label3.Size = new System.Drawing.Size(103, 23);
             this.label3.TabIndex = 8;
             this.label3.Text = "Project Name";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // textLog
+            // 
+            this.textLog.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.textLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textLog.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLog.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.textLog.Location = new System.Drawing.Point(16, 112);
+            this.textLog.Name = "textLog";
+            this.textLog.ReadOnly = true;
+            this.textLog.Size = new System.Drawing.Size(632, 424);
+            this.textLog.TabIndex = 10;
+            this.textLog.Text = "";
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 274);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.ClientSize = new System.Drawing.Size(665, 601);
+            this.Controls.Add(this.textLog);
             this.Controls.Add(this.btnOutputDirectoryPick);
             this.Controls.Add(this.btnInputFilePick);
             this.Controls.Add(this.editOutputDirectory);
@@ -157,11 +179,13 @@
             this.Controls.Add(this.editProjectName);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnGenerate);
-            this.Controls.Add(this.textLog);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "AHeadLib.Net";
             this.ResumeLayout(false);
@@ -177,10 +201,10 @@
         private System.Windows.Forms.Button btnOutputDirectoryPick;
         private System.Windows.Forms.TextBox editOutputDirectory;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textLog;
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.OpenFileDialog dialogInputFile;
         private System.Windows.Forms.TextBox editProjectName;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox textLog;
     }
 }
--- Sources/AHeadLib.Net/MainForm.cs
+++ Sources/AHeadLib.Net/MainForm.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
+using BlueMystic;
 using Microsoft.CodeAnalysis.CSharp;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AHeadLib.Net {
     public partial class MainForm : Form {
@@ -12,6 +15,7 @@ namespace AHeadLib.Net {
 
         public MainForm() {
             InitializeComponent();
+            _ = new DarkModeCS(this);
 
 #if DEBUG
             // for test only...
@@ -33,19 +37,37 @@ namespace AHeadLib.Net {
 
         private void analyzeFile(string fileName) {
             textLog.Text = "";
+            Log("");
+            textLog.AppendText($" AHeadLib.Net Modified | SakuraKooi {Environment.NewLine}", Color.Aqua);
+            Log("");
             editInputFile.Text = fileName;
             editProjectName.Text = Path.GetFileNameWithoutExtension(editInputFile.Text);
 
-            Log($" [*] Analyzing target file {Path.GetFileName(editInputFile.Text)} ...");
+            LogInfo($"Analyzing target file {Path.GetFileName(editInputFile.Text)} ...");
             string arch;
             PEFile.analyzeFile(editInputFile.Text, out exportNames, out arch);
-            Log($" [+] Export table has {exportNames.Count} entries: ");
+            if (exportNames.Count == 0) {
+                LogError("Error: Failed parse export table");
+                return;
+            }
+            
+            var names = exportNames;
+            names.RemoveAll(x => {
+                if (!SyntaxFacts.IsValidIdentifier(x) || x.Contains("@")) {
+                    LogWarn($"Skipped symbol: {x}");
+                    return true;
+                }
+                return false;
+            });
+
+            exportNames = names;
+            LogSuccess($"Export table has {exportNames.Count} entries: ");
             foreach (var name in exportNames) {
                 Log($"      - {name}");
             }
-            Log($" [+] Architecture: {arch}");
+            LogSuccess($"Target DLL's Architecture: {arch}");
             Log("");
-            Log(" [*] Ready to generate");
+            LogInfo("Ready to generate");
         }
 
         private void btnOutputDirectoryPick_Click(object sender, EventArgs e) {
@@ -57,34 +79,23 @@ namespace AHeadLib.Net {
         }
 
         private void btnGenerate_Click(object sender, EventArgs e) {
+            Log("");
             if (!File.Exists(editInputFile.Text)) {
-                MessageBox.Show("Input file is not exists.");
+                LogError("Error: Input file not exists");
                 return;
             }
 
             if (!Directory.Exists(editOutputDirectory.Text)) {
-                MessageBox.Show("Output Directory is not exists.");
+                LogError("Error: Output directory not exists");
                 return;
             }
 
             if (exportNames == null) {
-                MessageBox.Show("Failed get export table.");
+                LogError("Error: Failed parse export table");
                 return;
             }
 
-            var names = exportNames;
-            names.RemoveAll(x => {
-                if (!SyntaxFacts.IsValidIdentifier(x) || x.Contains("@")) {
-                    Log($"Skip symbol:{x}");
-
-                    return true;
-                }
-
-                return false;
-            });
-
-            exportNames = names;
-
+            LogInfo($"Writing project to {editOutputDirectory.Text}");
             VSProjectGenerator generator = new VSProjectGenerator(editOutputDirectory.Text,
                 Path.GetFileName(editInputFile.Text),
                 editProjectName.Text,
@@ -92,11 +103,37 @@ namespace AHeadLib.Net {
 
             generator.Write();
 
-            Log("Write Finished.");
+            LogSuccess("Project written.");
         }
 
         private void Log(string message) {
             textLog.AppendText(message + Environment.NewLine);
+        }
+        private void LogInfo(string message) {
+            textLog.AppendText($" [*] {message}{Environment.NewLine}", Color.WhiteSmoke);
+        }
+        private void LogWarn(string message) {
+            textLog.AppendText($" [*] {message}{Environment.NewLine}", Color.Yellow);
+        }
+        private void LogError(string message) {
+            textLog.AppendText($" [-] {message}{Environment.NewLine}", Color.Red);
+        }
+        private void LogSuccess(string message) {
+            textLog.AppendText($" [+] {message}{Environment.NewLine}", Color.GreenYellow);
+        }
+
+    }
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
+            box.ScrollToCaret();
         }
     }
 }
